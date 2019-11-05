bc4e384878d4e1f2c8a2921b01bcec20297f1ba9
Davi Bernardo <dnssites@gmail.com>
2019-11-05 16:24:43 -0300

    Baixando o Laravel 6

---
+++ resources/views/layouts/admin-lte/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block"><b>Versão</b> 1.0.2 </div>
+    <strong>Copyright &copy; 1987-2019 <a href="http://www.detro.rj.gov.br/" target="_blank">DETRO/RJ</a>.</strong> All rights reserved. Nova versão do sistema corporativo desenvolvido por setor <b>CODINF</b> a partir de 2019
+</footer>
